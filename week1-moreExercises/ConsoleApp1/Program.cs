498b0f8e629906f3893710c56fb61c95ad244852
wiwoucho <wiwoucho@gmail.com>
2024-09-11 21:49:52 +0100

    addition exercises

---
+++ week1-moreExercises/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿double degrees = double.Parse(Console.ReadLine());
+
+if (degrees > 25.9)
+{ Console.WriteLine("Hot"); }
+
+else if (degrees > 20)
+{ Console.WriteLine("Warm"); }
+
+else if (degrees > 14.9)
+{ Console.WriteLine("Mild"); }
+
+else if (degrees > 11.9)
+{ Console.WriteLine("Cool"); }
+
+else if (degrees > 4.9)
+{ Console.WriteLine("Cold"); }
+
+else
+{ Console.WriteLine("nknown"); }
+
+
+
---
+++ week1-moreExercises/perimeter_and_area_of_a_circle/Program.cs
@@ -0,0 +1,6 @@
+﻿double r = double.Parse(Console.ReadLine());
+
+double perimeter = 2 * Math.PI * r;
+double area = Math.PI * Math.Pow(r, 2);
+
+Console.WriteLine($"{area:f2}\n{perimeter:f2}");
