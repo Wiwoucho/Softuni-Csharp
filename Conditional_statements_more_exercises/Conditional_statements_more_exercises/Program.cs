1f8d2721715a92c214fb7b9b19bcc5ce4b2dfa21
wiwoucho <wiwoucho@gmail.com>
2024-09-12 20:29:14 +0100

    Conditional statement exercises

---
+++ Conditional_statements_more_exercises/Conditional_statements_more_exercises/Program.cs
@@ -0,0 +1,24 @@
+﻿int v = int.Parse(Console.ReadLine());
+int p1_fill_per_hour = int.Parse(Console.ReadLine());
+int p2_fill_per_hour = int.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
+double p1_litres_filled = p1_fill_per_hour * h;
+double p2_litres_filled = p2_fill_per_hour * h;
+double total_litres_filled = p1_litres_filled + p2_litres_filled;
+
+double total_percentage_filled = ((p1_litres_filled / v) * 100) + (p2_litres_filled / v) * 100;
+double p1_percentage_filled = (p1_litres_filled / total_litres_filled) * 100;
+double p2_percentage_filled = (p2_litres_filled / total_litres_filled) * 100;
+
+if (total_percentage_filled > 100)
+    {
+    double pool_overflow = total_litres_filled - v;
+    Console.WriteLine($"For {h} hours the pool overflows with {pool_overflow} liters.");
+    }
+else
+    {
+
+    Console.WriteLine($"The pool is {total_percentage_filled}% full. Pipe 1: {p1_percentage_filled:f2}%. Pipe 2: {p2_percentage_filled:f2}%.");
+    }
+
---
+++ Conditional_statements_more_exercises/sleepy_cat_tom/Program.cs
@@ -0,0 +1,19 @@
+﻿int days_for_break = int.Parse(Console.ReadLine());
+
+int break_days_norm = 127;
+int working_days_norm = 63;
+int norm = 30000;
+
+int total_time_for_playing = (break_days_norm * days_for_break) + ((365 - days_for_break) * working_days_norm);
+int overfill_the_norm = total_time_for_playing - norm;
+
+if (total_time_for_playing >= norm)
+    {
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine($"{overfill_the_norm / 60} hours and {overfill_the_norm % 60} minutes more for play");
+    }
+else
+    {
+    Console.WriteLine($"Tom sleeps well");
+    Console.WriteLine($"{(norm - total_time_for_playing) / 60} hours and {(norm - total_time_for_playing) % 60} minutes less for play");
+    }
