2286d847d1149fbcde1862a272bdddec22774206
wiwoucho <wiwoucho@gmail.com>
2024-09-13 22:30:56 +0100

    Adding Extra exercises

---
+++ Conditional_statements_more_exercises/harvest/Program.cs
@@ -0,0 +1,21 @@
+﻿int x = int.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+int workers_count = int.Parse(Console.ReadLine());
+
+double total_grapes_from_harvest = x * y;
+double wine_produced = (total_grapes_from_harvest * 0.4) / 2.5;  //2.5 is grapes needed for 1 liter of wine
+
+if (wine_produced >= z)
+
+    {
+    double liters_left = Math.Ceiling(wine_produced - z);
+    double workers_wine_liters_per_person = Math.Ceiling(liters_left / workers_count);
+    
+    Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine_produced)} liters.");
+    Console.WriteLine($"{Math.Ceiling(liters_left)} liters left -> {workers_wine_liters_per_person} liters per person.");
+    }
+else
+    {
+    Console.WriteLine($"It will be a tough winter! More {Math.Floor(z - wine_produced)} liters wine needed.");
+    }
---
+++ Conditional_statements_more_exercises/transport_price/Program.cs
@@ -0,0 +1,43 @@
+﻿int n = int.Parse(Console.ReadLine());
+string day_or_night = Console.ReadLine();
+
+double bus_price_per_km = 0.09;
+double train_price_per_km = 0.06;
+double taxi_starting_price = 0.70;
+
+if (day_or_night == "day")
+{
+    double taxi_price_per_km = 0.79;
+ 
+
+    if (n >= 100)
+    {
+        Console.WriteLine($"{train_price_per_km * n:f2}");
+    }
+    else if (n >= 20)
+    {
+        Console.WriteLine($"{bus_price_per_km * n:f2}");
+    }
+    else
+    {
+        Console.WriteLine($"{taxi_starting_price + (taxi_price_per_km * n):f2}");
+    }
+}
+
+if (day_or_night == "night")
+{
+    double taxi_price_per_km = 0.90;
+
+    if (n >= 100)
+    {
+        Console.WriteLine($"{train_price_per_km * n:f2}");
+    }
+    else if (n >= 20)
+    {
+        Console.WriteLine($"{bus_price_per_km * n:f2}");
+    }
+    else
+    {
+        Console.WriteLine($"{taxi_starting_price + (taxi_price_per_km * n):f2}");
+    }
+}
