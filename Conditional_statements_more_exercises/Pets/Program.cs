02690090c846e380d680337d9970d69b85a7f4c5
wiwoucho <wiwoucho@gmail.com>
2024-09-17 20:14:47 +0100

    adding additional homework

---
+++ Conditional_statements_more_exercises/Pets/Program.cs
@@ -0,0 +1,16 @@
+﻿int days = int.Parse(Console.ReadLine());
+double pets_food_kg = double.Parse(Console.ReadLine());
+double dog_food_kg = double.Parse(Console.ReadLine());
+double cat_food_kg = double.Parse(Console.ReadLine());
+double turtle_food_gr = double.Parse(Console.ReadLine());
+
+double total_food_kg = (dog_food_kg + cat_food_kg + (turtle_food_gr / 1000)) * days;
+
+if  (pets_food_kg >= total_food_kg)
+{
+    Console.WriteLine($"{Math.Floor(pets_food_kg - total_food_kg)} kilos of food left.");
+}
+else
+{
+    Console.WriteLine($"{Math.Ceiling(total_food_kg - pets_food_kg)} more kilos of food are needed.");
+}
