a3a7b6b95129b8efb6ee49e1ecbc2680afe9f0f5
wiwoucho <wiwoucho@gmail.com>
2024-09-26 22:49:11 +0100

    homework

---
+++ week3_conditional_statements_advanced_exercise/Ski_holiday/Program.cs
@@ -0,0 +1,60 @@
+﻿int daysForStaying = int.Parse(Console.ReadLine()) - 1;
+string roomType = Console.ReadLine();
+bool isPoisitive = Console.ReadLine() == "positive";
+
+double roomPrice = 18;
+double apartmentPrice = 25;
+double presidentApartmentPrice = 35;
+
+double totalPrice = 0;
+
+switch (roomType)
+{
+    case "room for one person":
+        totalPrice = roomPrice * daysForStaying;
+        break;
+    case "apartment":
+        totalPrice = daysForStaying * apartmentPrice;
+
+        if (daysForStaying < 10)
+        {
+            totalPrice *= 0.7; // 30% discount for less than 10 days
+        }
+        else if (daysForStaying >= 10 && daysForStaying <= 15)
+            totalPrice *= 0.65; // 35% discount between 10 and 15 days
+        else
+        {
+            totalPrice *= 0.5; // 50% discount for more than 15 days
+        }
+        break;
+
+    case "president apartment":
+
+        totalPrice = daysForStaying * presidentApartmentPrice;
+
+        if (daysForStaying < 10)
+        {
+            totalPrice *= 0.9; // 10% discount for less than 10 days
+        }
+        else if(daysForStaying >= 10 && daysForStaying<= 15)
+        {
+            totalPrice *= 0.85; // 15% discount between 10 and 15 days
+        }
+        else
+        {
+            totalPrice *= 0.8; // 20% discount for more than 15 days
+        }
+        break;
+}
+
+if (isPoisitive)
+{
+    totalPrice *= 1.25; // if he is happy with the stay he add additional 25% to the total price
+}
+else
+{
+    totalPrice *= 0.9; // if he is not happy reduce the total price with 10%
+}
+
+
+Console.WriteLine($"{totalPrice:f2}");
---
+++ week3_conditional_statements_advanced_exercise/operations_between_numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿double n1 = double.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine());
+var operators = Console.ReadLine();
+
+
+double result = 0;
+
+
+switch (operators)
+{
+    case "*":
+        result = n1 * n2;
+        Console.WriteLine($"{n1} {operators} {n2} = {result} - {(result % 2 == 0 ? "even" : "odd")}");
+        break;
+
+    case "-":
+        result = n1 - n2;
+        Console.WriteLine($"{n1} {operators} {n2} = {result} - {(result % 2 == 0 ? "even" : "odd")}");
+        break;
+
+    case "+":
+        result = n1 + n2;
+        Console.WriteLine($"{n1} {operators} {n2} = {result} - {(result % 2 == 0 ? "even" : "odd")}");
+        break;
+
+    case "/":
+        if (n2 == 0)
+        {
+            Console.WriteLine($"Cannot divide {n1} by zero");
+        }
+        else
+        {
+            result = n1 / n2;
+            Console.WriteLine($"{n1} {operators} {n2} = {result:f2}");
+        }
+        break;
+
+    case "%":
+        if (n2 == 0)
+        {
+            Console.WriteLine($"Cannot divide {n1} by zero");
+        }
+        else
+        {
+            result = n1 % n2;
+            Console.WriteLine($"{n1} {operators} {n2} = {(n1%n2)}");
+        }
+        break;
+
+}
+
+
