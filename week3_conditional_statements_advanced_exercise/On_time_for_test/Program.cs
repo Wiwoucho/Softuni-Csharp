e7cd42b65625b2d6cfdceefe4102b50778a36d4d
wiwoucho <wiwoucho@gmail.com>
2024-09-27 22:39:34 +0100

    homework

---
+++ week3_conditional_statements_advanced_exercise/On_time_for_test/Program.cs
@@ -0,0 +1,53 @@
+﻿int hourForTheTest = int.Parse(Console.ReadLine());
+int minutesForTheTest = int.Parse(Console.ReadLine());
+int arrivingStudentHour = int.Parse(Console.ReadLine());
+int arrivesStudentMinutes = int.Parse(Console.ReadLine());
+
+int hoursAndMinutesOfArriving = arrivingStudentHour * 60 + arrivesStudentMinutes;
+int hoursAndMinutesOfTest = hourForTheTest * 60 + minutesForTheTest;
+
+if (hoursAndMinutesOfArriving > hoursAndMinutesOfTest)
+{
+    Console.WriteLine("Late");
+
+    int difference = hoursAndMinutesOfArriving - hoursAndMinutesOfTest;
+
+    if (difference >= 60)
+    {
+        int hoursLate = difference / 60;
+        int minutesLate = difference % 60;
+        Console.WriteLine($"{hoursLate}:{minutesLate:D2} hours after the start");
+    }
+    else
+    {
+        Console.WriteLine($"{difference} minutes after the start");
+    }
+}
+else if (hoursAndMinutesOfTest - hoursAndMinutesOfArriving <= 30)
+{
+    Console.WriteLine("On time");
+
+    int difference = hoursAndMinutesOfTest - hoursAndMinutesOfArriving;
+
+    if (difference != 0)
+    {
+        Console.WriteLine($"{difference} minutes before the start");
+    }
+}
+else
+{
+    Console.WriteLine("Early");
+
+    int difference = hoursAndMinutesOfTest - hoursAndMinutesOfArriving;
+
+    if (difference >= 60)
+    {
+        int hoursEarly = difference / 60;
+        int minutesEarly = difference % 60;
+        Console.WriteLine($"{hoursEarly}:{minutesEarly:D2} hours before the start");
+    }
+    else
+    {
+        Console.WriteLine($"{difference} minutes before the start");
+    }
+}
---
+++ week3_conditional_statements_advanced_exercise/hotel_room/Program.cs
@@ -0,0 +1,49 @@
+﻿string month = Console.ReadLine();
+int daysForVacation = int.Parse(Console.ReadLine());
+
+double priceForApartment = 0;
+double priceForStudio = 0;
+double discount = 0;
+
+switch (month)
+{
+    case "May":
+    case "October":
+        priceForApartment = 65;
+        priceForStudio = 50;
+        break;
+
+    case "June":
+    case "September":
+        priceForApartment = 68.7;
+        priceForStudio = 75.2;
+        break;
+
+    case "July":
+    case "August":
+        priceForStudio = 76;
+        priceForApartment = 77;
+        break;
+
+}
+
+if (daysForVacation > 7 && daysForVacation <= 14 && (month == "May" || month == "October"))
+{
+    priceForStudio *= 0.95; // 5% discount for studio if the conditions are met
+}
+else if (daysForVacation > 14 && (month == "May" || month == "October"))
+{
+    priceForStudio *= 0.7; // 30% discont for studio if the conditions are met 
+}
+else if (daysForVacation > 14 && (month == "June" || month == "September"))
+{
+    priceForStudio *= 0.8; // 20% discont for studio if the conditions are met
+}
+
+if (daysForVacation > 14)
+{
+    priceForApartment *= 0.9; // 10% discont for apartment anytime when more than 14 days are booked
+}
+
+Console.WriteLine($"Apartment: {priceForApartment * daysForVacation:f2} lv.");
+Console.WriteLine($"Studio: {priceForStudio * daysForVacation:f2} lv.");
