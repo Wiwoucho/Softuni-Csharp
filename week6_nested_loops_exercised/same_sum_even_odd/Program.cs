1e19c247556f337ad20dc170e261289aab4e2481
wiwoucho <wiwoucho@gmail.com>
2024-10-15 13:18:33 +0300

    homework

---
+++ week6_nested_loops_exercised/same_sum_even_odd/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+for (int i = firstNum; i <= secondNum; i++)
+{
+    int sumOdd = 0;
+    int sumEven = 0;
+
+    for (int j = 0; j < i.ToString().Length; j++)
+    {
+        int currentDigit = int.Parse(i.ToString()[j].ToString());
+
+        if (j % 2 == 0)
+        {
+            sumEven += currentDigit;
+        }
+        else
+        {
+            sumOdd += currentDigit;
+        }
+
+    }
+    if (sumOdd == sumEven)
+    {
+        Console.Write($"{i} ");
+    }
+}
