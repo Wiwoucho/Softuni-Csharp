ee35f293d1b68696ea5761ca26ef44fe0ba1f76a
wiwoucho <wiwoucho@gmail.com>
2025-02-05 12:15:34 +0200

    additional exercises

---
+++ fundamentals/methodsMoreExercises/tribonacciSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿int fibNumber = int.Parse(Console.ReadLine());
+
+static void TribonacciSeq(int num)
+{
+    int[] sequence = new int[num];
+
+    
+
+    for (int i = 0; i < num; i++)
+    {
+        if (i < 2)
+        {
+            sequence[i] = 1;
+        }
+        else if (i == 2)
+        {
+            sequence[i] = 2;
+        }
+        else
+        {
+            sequence[i] = sequence[i-1 ] + sequence[i-2] + sequence[i-3];
+        }
+
+    }
+    Console.WriteLine(string.Join(" ", sequence));
+}
+
+TribonacciSeq(fibNumber);
